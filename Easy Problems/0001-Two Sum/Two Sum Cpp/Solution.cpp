dec330eb50c721c68518adcbf74ac086e251624e
grantbassler <gwbassler@gmail.com>
2026-04-13 11:32:43 -0700

    Two Sum Cpp solution

---
+++ Easy Problems/0001-Two Sum/Two Sum Cpp/Solution.cpp
@@ -0,0 +1,20 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(std::vector<int>& nums, int target) {
+            unordered_map<int, int> map;
+            int n = nums.size();
+            for (int i = 0; i < n; i++) {
+                int comp = target - nums[i];
+                if (map.count(comp)) {
+                    return {map[comp], i};
+                }
+                map[nums[i]] = i;
+            }
+            return{};
+        }
+};
---
+++ Easy Problems/0001-Two Sum/Two Sum Cpp/Solution.h
@@ -0,0 +1,16 @@
+//
+// Created by Grant Bassler on 4/13/26.
+//
+
+#ifndef LEETCODE_SOLUTIONS_SOLUTION_H
+#define LEETCODE_SOLUTIONS_SOLUTION_H
+
+
+
+class Solution {
+
+};
+
+
+
+#endif //LEETCODE_SOLUTIONS_SOLUTION_H
